5c1efc26129fa232cf01767a349d73f634f0c0d1
odunzechidubem <odunzemichael1273@gmail.com>
2024-08-02 13:12:33 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,98 @@
+export const robots =[
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Johnny Rex',
+        username: 'Rex',
+        email: 'odunzemichael1273@gmail.com'
+    }, 
+    {
+        id: 3,
+        name: 'Anvril May',
+        username: 'Anny',
+        email: 'anny@gmail.com'
+    }, 
+    {
+        id: 4,
+        name: 'Goldie Love',
+        username: 'Goldie',
+        email: 'goldie@ms.git'
+    }, 
+    {
+        id: 5,
+        name: 'Albert Ukaegbu',
+        username: 'Albertase',
+        email: 'albetukaegbu@gmail.com'
+    }, 
+    {
+        id: 6,
+        name: 'Shedrack Ajaegbu',
+        username: 'De Shedman',
+        email: 'shedrackajaegbu@yahoo.com'
+    }, 
+    {
+        id: 7,
+        name: 'Somto Nwosu',
+        username: 'Cardinal',
+        email: 'somtonwosu@raph.tsv'
+    }, 
+    {
+        id: 8,
+        name: 'Esther Amaka',
+        username: 'Stellamaris',
+        email: 'stellamaris@gmail.com'
+    }, 
+    {
+        id: 9,
+        name: 'Ikechukwu Nnoruka',
+        username: 'Hod',
+        email: 'ikhodman@git.ts'
+    }, 
+    {
+        id: 10,
+        name: 'Herald Mayor',
+        username: 'Gunner',
+        email: 'heraldman@org.co'
+    }, 
+    {
+        id: 11,
+        name: 'Nonso Aaron',
+        username: 'Striker',
+        email: 'heraldman@org.co'
+    }, 
+    {
+        id: 12,
+        name: 'Hamann Hurley',
+        username: 'Hurl',
+        email: 'heraldman@org.co'
+    }, 
+    {
+        id: 13,
+        name: 'Machine',
+        username: 'Macchi',
+        email: 'heraldman@org.co'
+    }, 
+    {
+        id: 14,
+        name: 'Nonso Aaron',
+        username: 'Striker',
+        email: 'heraldman@org.co'
+    }, 
+    {
+        id: 15,
+        name: 'Hamann Hurley',
+        username: 'Hurl',
+        email: 'heraldman@org.co'
+    }, 
+    {
+        id: 16,
+        name: 'Machine',
+        username: 'Macchi',
+        email: 'heraldman@org.co'
+    }, 
+] 
